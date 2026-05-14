04a53b0c08b2170d1f1fb6219800764cbbc60529
BSMARTAI <brian@bsmartenterprise.com>
2026-05-14 08:16:25 -0500

    Simplify translation route implementation
    Refactor translation route to use processTranslation controller.

--- routes/translate.routes.js
+++ routes/translate.routes.js
@@ -3,42 +3,22 @@ const express = require("express");
 const router = express.Router();
 
 const {
-  generateAccessibilityResponse,
-} = require("../services/watsonx");
+  processTranslation
+} = require("../controllers/translate.controller");
 
-router.post("/translate", async (req, res) => {
 
-  try {
 
-    const { text } = req.body;
+/*
+|--------------------------------------------------------------------------
+| TRANSLATION ROUTES
+|--------------------------------------------------------------------------
+*/
 
-    if (!text) {
-      return res.status(400).json({
-        error: "Text input is required",
-      });
-    }
+router.post(
+  "/translate",
+  processTranslation
+);
 
-    const response =
-      await generateAccessibilityResponse(text);
 
-    res.json({
-      success: true,
-      input: text,
-      output: response,
-    });
-
-  } catch (error) {
-
-    console.error(
-      "Translation route error:",
-      error.message
-    );
-
-    res.status(500).json({
-      success: false,
-      error: "Translation request failed",
-    });
-  }
-});
 
 module.exports = router;
